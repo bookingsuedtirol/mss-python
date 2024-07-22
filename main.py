0b5022f65ec944fa2954c366cc4097e70b3ce3f4
c-rubin <c-rubin@outlook.com>
2024-07-22 12:23:30 +0200

    refactor: unused functions removed

--- main.py
+++ main.py
@@ -7,53 +7,8 @@ load_dotenv()
 from os import getenv
 import xml.dom.minidom
 
-# from typing import Literal
-from dataclasses import dataclass, field
-
 from src.client import Client
 from src.request.types_mss import Credentials
-
-
-def make_xml_request(data: ET.Element, url: str, method: str) -> requests.Response:
-    """
-    Makes a post request to the given URL with the specified data and method name.
-
-    data: ElementTree with the data required in the XML
-    url: Service URL
-    method: Method to be executed by the service
-    """
-    ET.SubElement(data.find("header"), "method").text = method
-    return requests.post(
-        url,
-        data=ET.tostring(data, encoding="unicode"),
-        headers={"Content-Type": "application/xml"},
-    )
-
-
-def get_base_xml() -> ET.Element:
-    """
-    Returns an XML (ElementTree) with the mandatory elements needed for the request.
-    """
-    load_dotenv()
-    USER = getenv("MSS_SERVICE_USERNAME")
-    PASSWORD = getenv("MSS_SERVICE_PASSWORD")
-    SOURCE = getenv("MSS_SERVICE_SOURCE")
-    temp = {}
-    temp["user"] = USER
-    temp["password"] = PASSWORD
-    temp["source"] = SOURCE
-
-    root = ET.Element("root")
-    headers = ET.SubElement(root, "header")
-    credentials = ET.SubElement(headers, "credentials")
-    for x in temp:
-        ET.SubElement(credentials, x).text = temp[x]
-
-    ET.SubElement(
-        ET.SubElement(ET.SubElement(root, "request"), "search"), "lang"
-    ).text = "de"
-    # return ET.ElementTree(root)
-    return root
 
 
 def write_xml(root, filename):
@@ -83,26 +38,7 @@ def is_valid(xml: ET.Element, id=None, filename=None, file=False, print=False):
     return xml.find("header").find("result_id").text == expectedResultId.text
 
 
-def validator(x):
-    print("vali")
-    if x not in ["yellow", "blue"]:
-        raise ValueError(f"Invalid value: {x}")
-    return x
-
-
-@dataclass
-class A:
-    name2: str = field(default=None, metadata={"validaton": validator})
-    name: str = field(default=None)
-
-
 if __name__ == "__main__":
-    # try:
-    # resp = make_xml_request(get_base_xml(), getenv("MSS_SERVICE_URL"), "getHotelList")
-    # root = ET.fromstring(resp.content)
-
-    # print(is_valid(root, file=True, filename="./XML-samples/MSS_Hotel-List-1721202439420.xml"))
-
     # TODO testo klasen klient, implemento naj funxion qe tshtosh elementet te XML-ja (elements parameters at Clients constructor)
 
     credentials = Credentials(
@@ -111,8 +47,7 @@ if __name__ == "__main__":
         getenv("MSS_SERVICE_SOURCE"),
     )
     client = Client(credentials, "de")
-    # data = ET.Element("Header")
-    # SubElement()
+
     resp = client.request(None, getenv("MSS_SERVICE_URL"), "getHotelList")
 
     print(is_valid(resp, "407513bfca11b0591f9574b025d4caca"))
