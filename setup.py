09579e611316591272b41ae9242873c2f3746388
Tobias Burger <tobias_burger@hotmail.com>
2024-08-22 13:29:38 +0200

    feature: change project name

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="mss-python",  # Required
+    name="mss_python",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
